72687fe2854895162b4b7dc70695077fa3ea7ed6
Oleksander Skachkov <gskachkov@gmail.com>
2018-11-04 01:01:13 +0200

    Update conic-gradient-paint.js

--- conic-gradient-paint.js
+++ conic-gradient-paint.js
@@ -39,9 +39,9 @@ class ConicGradient {
         // o = o || {};
         var repeating = !!o.repeating;
 
-        // const size = o.size || Math.max(geom.width, geom.height);
+        const size = o.size || Math.max(geom.width, geom.height);
 	console.log(o.size, geom.width, geom.height);
-	const size = 948;
+	//const size = 948;
         let stops = (o.stops || "").split(/\s*,(?![^(]*\))\s*/); // commas that are not followed by a ) without a ( first
     
         let from = 0;
