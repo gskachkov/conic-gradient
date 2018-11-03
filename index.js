fa48e0081d0e2c09288382893e5457a95ceb744e
Oleksander Skachkov <gskachkov@gmail.com>
2018-11-03 16:45:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ $$("#examples article").forEach(function (article, i) {
 
     article.appendChild(textarea);
 });
+
+if (location.protocol === 'http:' && location.hostname !== 'localhost')
+    location.protocol = 'https:';
+if ('paintWorklet' in CSS) {
+    CSS.paintWorklet.addModule('conic-gradient-paint.js');
+} else {
+    document.body.innerHTML = 'You need support for <a href="https://drafts.css-houdini.org/css-paint-api/">CSS Paint API</a> to view this demo :(';
+}
