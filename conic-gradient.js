ebab0e704ff19226a2b5c541f17547643e19d9cc
Oleksander Skachkov <gskachkov@gmail.com>
2018-11-04 00:32:56 +0200

    Update conic-gradient.js

--- conic-gradient.js
+++ conic-gradient.js
@@ -157,7 +157,7 @@ _.prototype = {
 
 		var radius = this.r;
 		var x = this.size / 2;
-		console.log(r, x, size);
+		console.log(radius, x, size);
 
 		var stopIndex = 0; // The index of the current color
 		var stop = this.stops[stopIndex], prevStop;
